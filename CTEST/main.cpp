c7568e90d0f5f3b8d7bc7c3a3baf0f579ff4e145
Emrah Tekin <swissemrah@gmail.com>
2025-09-13 19:05:07 +0200

    CMakeList and main wurde erstellt

---
+++ CTEST/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <string>
+
+int main(int numberArgs, char* argumentStrings[]){
+    std::string argument = argumentStrings[0];
+    std::cout << argument << std::endl;
+}
